2c2c4619be85117add992a217c1c63fb8ab5af09
Alexandre Marques <marquesjurnioralexandre@gmail.com>
2023-04-11 14:33:52 -0300

    meu primeiro commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" /> 
+
+context('Funcionalidade login' , () =>{
+
+    it('Deve fazer login com sucesso' , () => {
+
+    })
+
+    it('deve exibir uma mensagem de erro ao inseir usuario ou senha invalido' , () =>{
+        
+    })
+})
